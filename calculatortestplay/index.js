65432d2f414e3c10addf239e13c0a863fb1943be
was1990 <rehmanw_22@hotmail.com>
2022-06-14 16:46:21 +0100

    FilterableList

--- calculatortestplay/index.js
+++
@@ -1,4 +0,0 @@
-const display = document.getElementById('display');
-buttons = document.querySelectorAll('button');
-
-buttons.addeventlistener  = onclick document.getElementsByclassName("button")[1].classList;
