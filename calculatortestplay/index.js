f2a6d312e489e21061189fadb818c9160048ab14
was1990 <rehmanw_22@hotmail.com>
2022-06-16 15:52:01 +0100

    APIs Examples added

---
+++ calculatortestplay/index.js
@@ -0,0 +1,26 @@
+const display1E1 = document.querySelector('.display-1');
+const display2E1 = document.querySelector('.display-2');
+const tempResultE1 = document.querySelector('.temp-result');
+const numbersE1 = document.querySelectorAll('.number');
+const operationE1 = document.querySelectorAll('.operation');
+const equalE1 = document.querySelector('.equal');
+const clearE1 = document.querySelector('.all-clear');
+const clearLastE1 = document.querySelector('.last-entity-clear');
+
+let dis1Num = '';
+let dis2Num = '';
+let result = null;
+let lastOperation = '';
+let haveDot = false;
+
+numbersE1.forEach( number => {
+    number.addEventListener('click', (e)=> {
+        if( e.target.innerText == '.' && !haveDot){
+            haveDot = true;
+        }else if(e.target.innerText === '.' && haveDot){
+            return;
+        }
+        dis2Num += e.target.innerText;
+        display2E1.innerText = dis2Num;
+    })
+})
