c228046ecea19636815d1a8aac269f9cf9b6137e
was1990 <rehmanw_22@hotmail.com>
2022-06-23 16:43:13 +0100

    Update to index.js

--- calculatortestplay/index.js
+++ calculatortestplay/index.js
@@ -15,7 +15,7 @@ let haveDot = false;
 
 numbersE1.forEach( number => {
     number.addEventListener('click', (e)=> {
-        if( e.target.innerText == '.' && !haveDot){
+        if( e.target.innerText  === '.' && !haveDot){
             haveDot = true;
         }else if(e.target.innerText === '.' && haveDot){
             return;
@@ -23,4 +23,27 @@ numbersE1.forEach( number => {
         dis2Num += e.target.innerText;
         display2E1.innerText = dis2Num;
     })
-})
+});
+
+operationE1.forEach( operation => {
+    operation.addEventListener('click', (e)=>{
+        if(!dis2Num) result;
+        haveDot= false;
+        const operationName = e.target.innerText;
+        if(dis1Num && dis2Num && lastOperation){
+            mathOperation();
+        }else{
+            result = parseFloat(dis2Num);
+        }
+        clearVar(operationName);
+        console.log(result);
+    })
+});
+
+function clearVar(name = ''){
+    dis1Num += dis2Num+ ' ' + name + ' ';
+    display1E1.innerText = dis1Num;
+    display2E1.innerText = '';
+    dis2Num = '';
+    tempResultE1.innerText = result;
+}
